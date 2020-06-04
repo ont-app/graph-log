43a63101e318b9000ee96f0024069d9d27f8fce2
Eric Scott <eric.scott@acm.org>
2020-06-03 17:08:04 -0700

    V. 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,24 +1,10 @@
 # Change Log
 All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
-## [Unreleased]
-### Changed
-- Add a new arity to `make-widget-async` to provide a different widget shape.
+## 0.1.1 - 2020-06-03
+- standard logging an graph logging are orthogonal
+  - adding a :glog/message statement will be sent to std logging even
+    if log-graph was never enabled.
+- Fixed some errors in quotation in value-* functions.
+- Timbre version upped to 4.11.0-alpha1
 
-## [0.1.1] - 2019-11-06
-### Changed
-- Documentation on how to make the widgets.
-
-### Removed
-- `make-widget-sync` - we're all async, all the time.
-
-### Fixed
-- Fixed widget maker to keep working when daylight savings switches over.
-
-## 0.1.0 - 2019-11-06
-### Added
-- Files from the new template.
-- Widget maker public API - `make-widget-sync`.
-
-[Unreleased]: https://github.com/your-name/graph-log/compare/0.1.1...HEAD
-[0.1.1]: https://github.com/your-name/graph-log/compare/0.1.0...0.1.1
--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
-(defproject ont-app/graph-log "0.1.1-SNAPSHOT"
+(defproject ont-app/graph-log "0.1.1"
   :description "An graph-based logging system for debugging"
   :url "https://github.com/ont-app/graph-log"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.520"]
+                 [org.clojure/clojurescript "1.10.597"]
                  [org.clojure/spec.alpha "0.2.176"]
                  ;; 3rd party libraries
                  [com.taoensso/timbre "4.11.0-alpha1"] ;; "4.10.0"] ;; basic logging
                  [cljstache "2.0.5"]
                  [lein-doo "0.1.11"] ;; cljs testing
                  ;;
-                 [ont-app/igraph "0.1.5-SNAPSHOT"]
-                 [ont-app/igraph-vocabulary "0.1.1-SNAPSHOT"] 
-                 [ont-app/vocabulary "0.1.1-SNAPSHOT"]
+                 [ont-app/igraph "0.1.5"]
+                 [ont-app/igraph-vocabulary "0.1.1"] 
+                 [ont-app/vocabulary "0.1.1"]
                  ]
   :plugins [[lein-codox "0.10.6"]
             [lein-cljsbuild "1.1.7"
