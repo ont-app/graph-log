c1a735585b970b895bfa98ecd9b89c0e9f9ab63a
Eric Scott <eric.scott@acm.org>
2021-03-08 05:08:04 -0800

    Removing SNAPSHOT from 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ont-app/graph-log "0.1.2-SNAPSHOT"
+(defproject ont-app/graph-log "0.1.2"
   :description "An graph-based logging system for debugging"
   :url "https://github.com/ont-app/graph-log"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
